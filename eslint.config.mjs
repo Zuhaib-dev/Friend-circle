c8f2d06fdc9a6246d15831343e2dcafc6d6b6a80
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-06-27 17:41:05 +0530

    chore: disable select eslint rules for react and typescript-eslint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react/no-unescaped-entities": "off",
+      "react/jsx-no-comment-textnodes": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@next/next/no-img-element": "off"
+    },
+  },
 ]);
 
 export default eslintConfig;
